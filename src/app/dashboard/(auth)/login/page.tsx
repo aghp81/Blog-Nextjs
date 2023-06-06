4704496cabff1fa7d9f554fd2a920b1cb463e9a0
abol ghana <aghp81@gmail.com>
2023-06-06 14:01:09 +0430

    صفحه ورود

---
+++ src/app/dashboard/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from '../../../../app/page.module.css'
+
+export default function Login() {
+  return (
+    <main className={styles.main}>
+      <div> ورود</div>
+    </main>
+  )
+}
