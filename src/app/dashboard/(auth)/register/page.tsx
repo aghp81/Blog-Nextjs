181368a6533b9facd584c20cc5569cd525e1be94
abol ghana <aghp81@gmail.com>
2023-06-06 14:02:58 +0430

    ثبت نام

---
+++ src/app/dashboard/(auth)/register/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from '../../../../app/page.module.css'
+
+export default function Register() {
+  return (
+    <main className={styles.main}>
+      <div>ثبت نام </div>
+    </main>
+  )
+}
