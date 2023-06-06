d6bf38ac2f2c93f704c1f1d04ac11fd4e173565c
abol ghana <aghp81@gmail.com>
2023-06-06 14:06:38 +0430

    ایجاد فوتر و نوبار در پوشه کامپوننت

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+import styles from '../../../app/page.module.css'
+
+export default function Footer() {
+  return (
+    <main className={styles.main}>
+      <div>فوتر  </div>
+    </main>
+  )
+}
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+import styles from '../../../app/page.module.css'
+
+export default function Navbar() {
+  return (
+    <main className={styles.main}>
+      <div>Navbar  </div>
+    </main>
+  )
+}
