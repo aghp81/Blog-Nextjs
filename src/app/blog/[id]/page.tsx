5fbdfd92838844ce052b5084217ded4831e66482
abol ghana <aghp81@gmail.com>
2023-06-06 12:37:28 +0430

    ایجاد صفحه بلاگ

---
+++ src/app/blog/[id]/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+import styles from '../../../app/page.module.css'
+
+export default function BlogPost() {
+  return (
+    <main className={styles.main}>
+      <div>پست وبلاگ </div>
+    </main>
+  )
+}
