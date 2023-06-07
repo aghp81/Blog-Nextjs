2c5431a2a62609f14f194c13e1370db8770ea19c
abol ghana <aghp81@gmail.com>
2023-06-07 14:06:00 +0430

    اصلاح متن فوتر

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,7 +5,8 @@ import styles from '../../app/page.module.css'
 export default function Footer() {
   return (
     <main className={styles.main}>
-      <div> @Abol Ghana  </div>
+      <div> 2023 Abol Ghana. All Right Reserved.  </div>
+      <div></div>
     </main>
   )
 }
