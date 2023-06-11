5c40d28db044a12e0034b270f495dc9541375509
abol ghana <aghp81@gmail.com>
2023-06-11 11:57:30 +0430

    ایجاد صفحه برای رندر سمت کلاینت

---
+++ src/app/csrpage/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from '../page.module.css'
+
+export default function CsrPage() {
+  return (
+    <main className={styles.main}>
+      <div>CsrPage </div>
+    </main>
+  )
+}
