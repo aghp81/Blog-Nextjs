db9703726782d39106d0d391262c3a4dbb63a3ea
abol ghana <aghp81@gmail.com>
2023-06-11 11:57:49 +0430

    ایجاد صفحه برای رندر سمت سرور

---
+++ src/app/ssrpage/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from '../page.module.css'
+
+export default function SsrPage() {
+  return (
+    <main className={styles.main}>
+      <div>SsrPage </div>
+    </main>
+  )
+}
