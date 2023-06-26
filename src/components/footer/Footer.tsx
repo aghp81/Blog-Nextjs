b7b9badfd560476411fe3452c3b4b3086b639e20
abol ghana <aghp81@gmail.com>
2023-06-26 17:32:55 +0330

    قرار دادن تصویر در فوتر و پوشه public

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,15 +6,15 @@ import styles from './footer.module.css'
 
 export default function Footer() {
   return (
-    <main className={style.main}>
+    
       <div className={styles.container}>
         <div > 2023 Abol Ghana. All Right Reserved.  </div>
         <div>
-          
+          <Image src="/Instagram.png" width={15} height={15} alt="Aghp"  />
         </div>
       </div>
       
       
-    </main>
+    
   )
 }
