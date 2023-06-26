89a579e0e66d0cf5ab855f4ef020317a10bc5ab9
abol ghana <aghp81@gmail.com>
2023-06-26 16:39:32 +0330

    ایجاد استایل جداگانه برای داشبورد

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
-import styles from '../page.module.css'
+import style from '../page.module.css'
+import styles from './page.module.css'
+
 
 export default function Dashboard() {
   return (
-    <main className={styles.main}>
-      <div>داشبورد </div>
+    <main className={style.main}>
+      <div className={styles.container}>داشبورد </div>
     </main>
   )
 }
