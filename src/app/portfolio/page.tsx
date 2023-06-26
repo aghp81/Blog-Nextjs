b5016d546ec7324cf1ba51779c1c84bf65df7db8
abol ghana <aghp81@gmail.com>
2023-06-26 16:40:56 +0330

    ایجاد استایل جداگانه برای پورتفولیو

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
-import styles from '../page.module.css'
+import style from '../page.module.css'
+import styles from './page.module.css'
+
 
 export default function Portfolio() {
   return (
-    <main className={styles.main}>
-      <div> پورتفولیو</div>
+    <main className={style.main}>
+      <div className={styles.container}> پورتفولیو</div>
     </main>
   )
 }
