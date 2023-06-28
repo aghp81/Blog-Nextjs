689a830177e92ea3e398f565b47213b71ea3e63b
abol ghana <aghp81@gmail.com>
2023-06-28 16:27:05 +0330

    صفحه about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,8 +5,34 @@ import styles from './page.module.css'
 
 export default function About() {
   return (
-    <main className={style.main}>
-      <div className={styles.container}>درباره ما</div>
-    </main>
+      <div className={styles.container}>
+        <div className={styles.imgContainer}>
+          <Image 
+          src="https://www.monash.edu/__data/assets/image/0020/3214271/Banner_LearnDigitalStorytelling.jpg" 
+          fill={true} 
+          alt=""  
+          className={styles.img}
+          />
+          <div className={styles.imgText}>
+            <h1 className={styles.imgTitle}>Digital StoryTellers</h1>
+            <h2 className={styles.imgDesc}>Handcrafting award winning digital experinces</h2>
+          </div>
+        </div>
+        <div className={styles.textContainer}>
+          <div className={styles.item}>
+            <h1 className={styles.title}>Who Are We?</h1>
+            <p className={styles.desc}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi architecto quae cumque quo quibusdam
+              <br />
+              <br />
+              corporis omnis fugit reiciendis placeat? Adipisci nesciunt ab tempora neque beatae laudantium nobis quibusdam officiis. Placeat?
+              </p>
+          </div>
+          <div className={styles.item}>
+            <h1 className={styles.title}>What We Do?</h1>
+          </div>
+        </div>
+        
+      </div>
   )
 }
