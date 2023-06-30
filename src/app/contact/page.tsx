544303d4c32532bb00d637b7fc497e073f65e900
abol ghana <aghp81@gmail.com>
2023-06-30 09:38:16 +0330

    صفحه Aboute و استایل دهی به آن

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,12 +1,29 @@
-import Image from 'next/image'
-import style from '../page.module.css'
-
+import Button from '@/components/Button/Button'
 import styles from './page.module.css'
+import Image from 'next/image'
+import Cimage from 'public/Contact.png'
+
 
 export default function Contact() {
   return (
-    <main className={style.main}>
-      <div className={styles.container}>تماس ما</div>
-    </main>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Let's Keep In Touch </h1>
+        <div className={styles.content}>
+          <div className={styles.imgContainer}>
+            <Image
+            src={Cimage}
+            alt=""
+            fill={true}
+            className={styles.image}
+            />
+          </div>
+          <form className={styles.form}>
+            <input type="text" placeholder="name" className={styles.input} />
+            <input type="text" placeholder="email" className={styles.input} />
+            <textarea className={styles.textArea} placeholder="message" ></textarea>
+            <Button url="#" text="send" />
+          </form>
+        </div>
+      </div>
   )
 }
