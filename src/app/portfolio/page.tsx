406b99c2b52dd014416090e18ec1bd304799b721
abol ghana <aghp81@gmail.com>
2023-07-08 15:49:41 +0330

    تصاویر بخش پورتفولیو

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,12 +1,22 @@
-import Image from 'next/image'
-import style from '../page.module.css'
+import Link from 'next/link'
 import styles from './page.module.css'
 
 
 export default function Portfolio() {
   return (
-    <main className={style.main}>
-      <div className={styles.container}> پورتفولیو</div>
-    </main>
+      <div className={styles.container}>
+        <h1 className={styles.selectTitle}>Choos a gallery</h1>
+        <div className={styles.items}>
+          <Link href="/portfolio/illustrations" className={styles.item}>
+            <span className={styles.title}>Illustrations</span>
+          </Link>
+          <Link href="/portfolio/websites" className={styles.item}>
+            <span className={styles.title}>Websites</span>
+          </Link>
+          <Link href="/portfolio/application" className={styles.item}>
+            <span className={styles.title}>Application</span>
+          </Link>
+        </div>
+      </div>
   )
 }
