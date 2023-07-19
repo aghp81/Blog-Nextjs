d65df8535dbe55c100d5c3cf4066dfd2878a2b0f
abol ghana <aghp81@gmail.com>
2023-07-19 16:00:02 +0330

    ایجاد Dark Mode و استایل آن

--- src/components/DarkModeToggle/DarkModeToggle.tsx
+++ src/components/DarkModeToggle/DarkModeToggle.tsx
@@ -3,7 +3,9 @@ import styles from './DarkModeToggle.module.css'
 export default function DarkModeToggle() {
     return (
         <div className={styles.container}>
-            
+            <div className={styles.icon}>🌙</div>
+            <div className={styles.icon}>🌕</div>
+            <div className={styles.ball} />
         </div>
     )
   }
