4a3b5f6d087ee016ed9c1c82283a2ad439f9f084
abol ghana <aghp81@gmail.com>
2023-07-22 15:50:03 +0330

    dark mode

--- src/components/DarkModeToggle/DarkModeToggle.tsx
+++ src/components/DarkModeToggle/DarkModeToggle.tsx
@@ -1,11 +1,14 @@
 import styles from './DarkModeToggle.module.css'
 
 export default function DarkModeToggle() {
+
+    const mode : string = "dark";
     return (
         <div className={styles.container}>
             <div className={styles.icon}>🌙</div>
             <div className={styles.icon}>🌕</div>
-            <div className={styles.ball} />
+            <div className={styles.ball} 
+            style={mode === "light" ? { left:"2px" } : { right:"2px" }} />
         </div>
     )
   }
