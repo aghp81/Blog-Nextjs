b9cfe21fdbd1783e4d925ea569f815611d3d4347
abol ghana <aghp81@gmail.com>
2023-08-15 10:43:20 +0330

    data for portfolio

---
+++ src/app/portfolio/[category]/data.js
@@ -0,0 +1,58 @@
+export const items ={
+    applications: [
+        {
+            id: 1,
+            title: "Creative Portfolio",
+            desc: "lorem ipsum",
+            image: "",
+        },
+        {
+            id: 2,
+            title: "Creative Portfolio",
+            desc: "lorem ipsum",
+            image: "",
+        }
+
+    ],
+
+    illustrations: [
+        {
+            id: 1,
+            title: "Creative Portfolio",
+            desc: "lorem ipsum",
+            image: "",
+        },
+        {
+            id: 2,
+            title: "Creative Portfolio",
+            desc: "lorem ipsum",
+            image: "",
+        }
+
+    ],
+
+    websites: [
+        {
+            id: 1,
+            title: "Creative Portfolio",
+            desc: "lorem ipsum",
+            image: "",
+        },
+        {
+            id: 2,
+            title: "Creative Portfolio",
+            desc: "lorem ipsum",
+            image: "",
+        },
+        {
+            id: 3,
+            title: "Creative Portfolio",
+            desc: "lorem ipsum",
+            image: "",
+        }
+
+    ],
+
+
+
+}
