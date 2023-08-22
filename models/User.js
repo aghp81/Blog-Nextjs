b504598ea1666243bed65f1cd1b4e5aa2f4affdc
abol ghana <aghp81@gmail.com>
2023-08-22 08:34:18 +0330

    for user table

---
+++ models/User.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose"
+import { stringify } from "querystring"
+
+const { Schema } = mongoose
+
+const userSchema = new Schema({
+    name:{
+        type: string,
+        unique: true, // for username
+        required: true, 
+    },
+    email:{
+        type: string,
+        unique: true, // for email
+        required: true, 
+    },
+    password:{
+        type: string,
+        required: true, 
+    },
+    
+}, 
+ {timestamp: true } // created_at , updated_at
+)
